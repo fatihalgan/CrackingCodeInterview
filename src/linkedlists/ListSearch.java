405ff176109e1bd91c15b0b681323daf7fed656f
falgan <fatih.algan@gmail.com>
2016-05-02 14:31:27 +0300

    # WARNING: head commit changed in the meantime
    Initial Commit

--- src/linkedlists/ListSearch.java
+++ src/linkedlists/ListSearch.java
@@ -42,4 +42,42 @@ public class ListSearch {
 		}
 		return true;
 	}
+	
+	//Given a circular linked list, implement an algorithm wihch returns the node
+	//at the beginning of the loop
+	// A -> B -> C -> D -> E -> C (The same C as earlier) Output: C
+	//Solution:
+	// 1 - Create two pointers, FastPointer and SlowPointer
+	// 2 - Move fast pointer at a rate of 2 steps and SlowPointer at a rate of 1 step
+	// 3 - When they collide, move SlowPointer to LinkedListHead. Keep fast pointer where it is
+	// 4 - Move fast pointer and slow pointer at a rate of 1 step. Return the new collision point.
+	Node<Integer> findBeginning(Node<Integer> head) {
+		Node<Integer> slow = head;
+		Node<Integer> fast = head;
+		
+		/* Find meeting point. This will be LOOP_SIZE - k steps into the
+		 * linked list. */
+		while(fast != null && fast.next != null) {
+			slow = slow.next;
+			fast = fast.next.next;
+			if(slow == fast) break;
+		}
+		
+		/* Error check - no meeting point, and therefore no loop */
+		if (fast == null || fast.next == null) {
+			return null;
+		}
+		
+		/* Move slow to Head. Keep fast at Meeting Point. Each are k
+		 * steps from the Loop Start. If they move at the same pace,
+		 * they must meet at Loop Start. */
+		slow = head;
+		while(slow != fast) {
+			slow = slow.next;
+			fast = fast.next;
+		}
+		/* Both now point to the start of the loop. */
+		return fast;
+	}
+	
 }
